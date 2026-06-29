ed0f4acbfc9a6d7e539d68019aa59c4a763b0b33
Ivannia <ivallen84@gmail.com>
2026-06-29 15:37:07 -0600

    se establecen cambios dentro de cabana
    se crean metodos en canaba

--- src/Cabana/cabanas.java
+++ src/Cabana/cabanas.java
@@ -9,5 +9,64 @@ package Cabana;
  * @author Student
  */
 public class cabanas {
+      protected double tamaño;
+    protected final double impuesto =13/100;
+    protected String piscina;
+    protected double mCuadrados;
+    protected double precioTOT;
+    protected final double cantNoches = 20;
+
+    public double getTamaño() {
+        return tamaño;
+    }
+
+    public double getImpuesto() {
+        return impuesto;
+    }
+
+    public String getPiscina() {
+        return piscina;
+    }
+
+    public double getmCuadrados() {
+        return mCuadrados;
+    }
+
+    public double getPrecioTOT() {
+        return precioTOT;
+    }
+
+    public cabanas(double tamaño, String piscina, double mCuadrados, double precioTOT) {
+        this.tamaño = tamaño;
+        this.piscina = piscina;
+        this.mCuadrados = mCuadrados;
+        this.precioTOT = precioTOT;
+    }
+    
+    public void cabanaTam(){
+        if(mCuadrados>=80){
+            System.out.print("Cabaña amplia");
+           
+        }else {
+            System.out.print("Cabaña no amplia");
+        }      
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
