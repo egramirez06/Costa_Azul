b5303c5e6984971be4e48839c43ba95d99c2bb33
Ivannia <ivallen84@gmail.com>
2026-06-29 15:28:37 -0600

    se guardan cambios

--- src/Alojamiento/alojamientos.java
+++ src/Alojamiento/alojamientos.java
@@ -9,5 +9,36 @@ package Alojamiento;
  * @author Student
  */
 public class alojamientos {
+     protected int codigo;
+    protected String nombre;
+    protected int capacidad;
+    protected final int precioNoche = 100;
+    protected boolean disponibilidad;
+    protected String estado;
+    
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public int getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
     
 }
