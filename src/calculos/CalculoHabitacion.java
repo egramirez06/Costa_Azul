38ff27108e615d96e9b4447270bd86f186ca39f3
stef <esramirezgo@est.utn.ac.cr>
2026-06-29 16:14:58 -0600

    Se hizo clase clientes para realizar un calculo

---
+++ src/calculos/CalculoHabitacion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculos;
+import habitaciones.Habitacion;
+/**
+ *
+ * @author Student
+ */
+public class CalculoHabitacion {
+    private Habitacion[] habitacion;
+    
+    public boolean calcularHabitaionDisponible(boolean disponible){
+        for(int i = 0;i < habitacion.length; i++){
+        if(habitacion[i] != null){
+            if(habitacion[i].isDisponibilidad()==disponible){
+                return true;
+            }
+        }
+            }
+        return false;
+    }
+    
+    
+    
+    
+}
