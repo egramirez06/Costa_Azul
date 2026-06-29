51378fe3236683ca55d81c732cc8a22e5de811a6
stef <esramirezgo@est.utn.ac.cr>
2026-06-29 15:24:49 -0600

    Se modifico clase clientes
    Se creo con sus metodos, set, get, constructor y toString

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -9,5 +9,51 @@ package clientes;
  * @author Student
  */
 public class Cliente {
+    private String nombre;
+    private String identificacion;
+    private String telefono;
+    private String correo;
+    private String pais;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Cliente(String nombre, String identificacion, String telefono, String correo, String pais) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.pais = pais;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", identificacion=" + identificacion + ", telefono=" + telefono + ", correo=" + correo + ", pais=" + pais + '}';
+    }
     
 }
